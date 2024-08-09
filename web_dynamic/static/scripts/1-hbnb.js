2b9e1487f7c6cfad4d186c6422631e0ec956b7f4
danimohli <danimohlife@gmail.com>
2024-08-09 13:19:00 +0100

    jquerie

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,18 @@
+/* global $ */
+$(document).ready(function () {
+  const selectedAmenities = {};
+
+  $('input[type="checkbox"]').change(function () {
+    const amenityId = $(this).attr('data-id');
+    const amenityName = $(this).attr('data-name');
+
+    if ($(this).is(':checked')) {
+      selectedAmenities[amenityId] = amenityName;
+    } else {
+      delete selectedAmenities[amenityId];
+    }
+
+    const amenityNames = Object.values(selectedAmenities).join(', ');
+    $('div.amenities h4').text(amenityNames);
+  });
+});
